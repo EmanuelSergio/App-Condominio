c7e55fbcbf00862a81f5b15d81830ca6cc0f83d5
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2023-11-29 23:35:14 -0300

    add

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,13 +16,21 @@ import {MatButtonModule} from '@angular/material/button';
 import {NgIf} from '@angular/common';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatIconModule} from '@angular/material/icon';
+import { ProductComponent } from './components/product/product.component';
+import { ViewsComponent } from './views/views.component';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    ProductComponent,
+    ViewsComponent,
+    HomeComponent,
+    ProductCrudComponent
   ],
   imports: [
     BrowserModule,
